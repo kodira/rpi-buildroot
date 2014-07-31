2a6168d4f2ee614ad9f70fe4d7d82ea0df285092
Cornelius Hald <cornelius.hald@kodira.de>
2014-07-31 19:38:40 +0200

    Set locale, keymap and timezone

--- board/raspberrypi/postscript.sh
+++ board/raspberrypi/postscript.sh
@@ -8,5 +8,10 @@ cp ${TARGET}/../build/linux-*/System.map ${TARGET}/System.map
 # copy kernel
 cp ${TARGET}/../images/zImage ${TARGET}/../images/boot/kernel.img
 
+# set timezone
+#ln -s ${TARGET}/usr/share/zoneinfo/Europe/Berlin ../../../../etc/localtime
+# Creating relative links seems to be difficult here
+cp ${TARGET}/usr/share/zoneinfo/Europe/Berlin ${TARGET}/etc/localtime
+
 exit 0
 
